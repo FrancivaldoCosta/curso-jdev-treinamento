c179bd4dd7f4efd1b5d517a3f8dc2a4648e93a95
Francivaldo Costa <francivaldoaccosta@gmail.com>
2025-03-27 01:18:36 -0300

    Criando meu primeiro projeto java

---
+++ primeiro_programa_java/src/curso_java_executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package curso_java_executavel;
+
+public class PrimeiraClasseJava {
+
+	
+	//Main é um metodo auto-executavel em java
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro programa em java - do Curso Formação Web");
+		System.out.println("Java é orientado a objetos");
+		
+	}
+
+}
