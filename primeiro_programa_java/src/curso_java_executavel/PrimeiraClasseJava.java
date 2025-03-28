762e852b3f02ed6fed1bbad5a2c6d3c870d7d3be
Francivaldo Costa <francivaldoaccosta@gmail.com>
2025-03-28 08:25:49 -0300

    Fazendo teste de revert

--- primeiro_programa_java/src/curso_java_executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/curso_java_executavel/PrimeiraClasseJava.java
@@ -2,15 +2,14 @@ package curso_java_executavel;
 
 public class PrimeiraClasseJava {
 
-	
-	//Main é um metodo auto-executavel em java
+	// Main é um metodo auto-executavel em java
 	public static void main(String[] args) {
-		
+
 		System.out.println("Meu primeiro programa em java - do Curso Formação Web");
 		System.out.println("Java é orientado a objetos");
-		
-		
-		
+
+		System.out.println("Teste de commit");
+
 	}
 
 }
