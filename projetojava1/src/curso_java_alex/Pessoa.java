9745a5b86f31adfec629877cb94bce1c215f3b85
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-05-04 20:55:30 -0300

    Código da tarefa1 da branch1

--- projetojava1/src/curso_java_alex/Pessoa.java
+++ projetojava1/src/curso_java_alex/Pessoa.java
@@ -10,7 +10,7 @@ public class Pessoa {
 	private Date dataNascimento;
 	private Date dataCadastro;
 
-			
+	private String testeBrench;
 	
 	public Date getDataNascimento() {
 		return dataNascimento;
