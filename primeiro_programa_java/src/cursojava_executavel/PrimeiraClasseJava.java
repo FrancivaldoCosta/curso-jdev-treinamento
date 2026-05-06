122e93893fe39f879456f156abff54d74e06d152
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-05-06 02:24:39 -0300

    Terminando aulas 05-05

--- primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
@@ -1,25 +1,29 @@
 package cursojava_executavel;
 
-
 public class PrimeiraClasseJava {
-	
+
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
-		
-		int maiorIdade = 18;
-		
-		int idosoIdade = 60;
-		
-		int jovemIdade = 16;
-		
-		int criancaIdade = 7;
-		
-		String cpf = "123434121321";
-		
-		System.out.println("Maior idade é = " + maiorIdade);
-		System.out.println("Idoso idade é = " + idosoIdade);
-		
-		
+
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 10;
+		int nota4 = 10;
+
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aluno aprovado " + media);
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperação " + media);
+		}
+
+		else {
+			System.out.println("Aluno reprovado " + media);
+		}
+
 	}
 
 }
