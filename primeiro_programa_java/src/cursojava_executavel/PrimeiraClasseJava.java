43d6153045b30be454ba44cfa4b6f0c44675d030
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-05-07 01:59:20 -0300

    Alterações do dia

--- primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
@@ -5,23 +5,30 @@ public class PrimeiraClasseJava {
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		int nota1 = 10;
-		int nota2 = 60;
-		int nota3 = 10;
-		int nota4 = 10;
+		int nota1 = 93;
+		int nota2 = 90;
+		int nota3 = 90;
+		int nota4 = 90;
 
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-		if (media >= 70) {
-			System.out.println("Aluno aprovado " + media);
-		} else if (media >= 40 && media <= 69) {
-			System.out.println("Aluno em recuperação " + media);
-		}
+		/* Operações lógicas aninhadas: São operações dentro de operações */
 
-		else {
-			System.out.println("Aluno reprovado " + media);
+		if (media >= 50) {
+			if (media >= 70) {
+				if (media > 90) {
+					System.out.println("Aluno aprovado direto - Parabéns " + media);
+				} else {
+					System.out.println("Aluno aprovado direto " + media);
+				}
+
+			} else {
+				System.out.println("Aluno em recuperação " + media);
+			}
+		} else {
+			System.out.println("Aluno reprovado direto " + media);
 		}
 
 	}
