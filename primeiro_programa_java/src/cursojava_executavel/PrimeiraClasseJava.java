f16a7c4d14db99183ed67eaf6eacb993b009dd14
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-05-13 00:10:34 -0300

    Terminando modulo 8

--- primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/cursojava_executavel/PrimeiraClasseJava.java
@@ -7,34 +7,32 @@ public class PrimeiraClasseJava {
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-	String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
-	String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
-	
-	double carroNumero = Double.parseDouble(carros); 
-	double pessoaNumero = Double.parseDouble(pessoas); 
-	
-	int divisao = (int) (carroNumero / pessoaNumero);
-	
-	double resto = (carroNumero % pessoaNumero);
-	
-	int resposta =  JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão? ");
-	
-	if (resposta == 0) {
-		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carro");
-		
-	} else {
-		JOptionPane.showMessageDialog(null, "Não quis ver o RESULTADO");
-	}
-	
-	resposta =  JOptionPane.showConfirmDialog(null, "Deseja ver o RESTO da divisão? ");
-	
-	if ( resposta == 0) {
-		JOptionPane.showMessageDialog(null, "O RESTO da divisão é " + resto);
-	} else {
-		JOptionPane.showMessageDialog(null, "Não quis ver o RESTO");
-	}
-	
-	
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
+
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+
+		/* Média para aprovação é 70 */
+
+		if (media >= 50) {
+			if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com média de : " + media);
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Aluno está em recuperação com média de : " + media);
+			}
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Aluno está reprovado com média de : " + media);
+		}
+
 	}
 
 }
